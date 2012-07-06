b34a78f6ffc39f27802c24832c6b3ba1252d3ad7
Erika Brice and Garston Tremblay <pair+eb+gt@rallydev.com>
2012-07-06 16:13:07 -0600

    Example Ruby script for Rally email gateway

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 RallyEmailGateway
 =================
+README file for creating a Rally defect or story using a POP3 mail server
+
+Install rubygems (from command line or terminal: gem install rubygems)
+Install mail (from command line or terminal: gem install mail)
+Install rally_rest_api (from command line or terminal: gem install rally_rest_api)
+
+Create email account to be used to process all incoming Rally creation requests. For example, rallyrequest@domain.com.
+
+Setup the Ruby script on a server to run a service when an incoming mail is sent to the email account created above.
+The Ruby script will create defects for email subjects starting with 'defect'. Otherwise, a user story will be created.
+
+Edit Ruby script with appropriate Rally settings and POP3 mail server settings.
+Rally user needs to have user permissions to the project specified in the script.
+
+Create a cron job on a Unix/Mac server to run the script at a set interval or the equivalent for Windows. 
---
+++ rally_email_gateway.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'net/pop'
+require 'mail'
+require 'rally_rest_api'
+
+# Setup app information
+custom_headers = CustomHttpHeader.new
+custom_headers.name = 'Mail 2 Rally'
+custom_headers.version = '0.1'
+custom_headers.vendor = 'Rally Software'
+
+#Rally settings
+rally_server = 'https://rally1.rallydev.com/slm'
+rally_username = 'username@domain.com'
+rally_pwd = 'mypwd'
+rally_workspace = 'Workspace 1'
+rally_project = 'Sample Project'
+
+rally = RallyRestAPI.new(:base_url => rally_server,
+    :username => rally_username,
+    :password => rally_pwd,
+    :workspace => rally_workspace,
+    :project => rally_project,
+    :http_headers => custom_headers)
+
+#POP3 mail server settings
+mail_server = 'pop.gmail.com'
+mail_port = '995'
+mail_username = 'username@domain.com'
+mail_pwd = 'mypwd'
+    
+pop = Net::POP3.new(mail_server, mail_port)
+pop.enable_ssl
+pop.start(mail_username, mail_pwd) 
+if pop.mails.empty?
+  puts 'No mail.'
+else
+  pop.each_mail do |m| 
+    mail = Mail.new(m.pop)
+    rally.create(mail.subject.downcase.start_with?("defect") ? :defect : :hierarchical_requirement, :name => mail.subject, :description => mail.html_part.body)
+    m.delete
+  end
+end
+pop.finish
+
